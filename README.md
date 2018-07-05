8a8fcf93fc1b4c64402b83a5c9b8fb67145cb768
John Perri Cruz <jcruz@optimizex.com>
2018-07-05 12:44:30 -0700

    Initial Upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Magento 1 Helper Class
+You can always add your custom function in here. Happy Programming!
+
+Here's some code! :+1:
+```html
+use via phtml templates : 
+Mage::helper('Primeview_Helper')->debug();
+
+```
+<br/><br/>
+<b>John Perri Cruz</b><br/>
+https://www.johnperricruz.com
+
+ 
---
+++ app/code/local/Jpc/Mage/Helper/Data.php
@@ -0,0 +1,17 @@
+<?php
+
+class Jpc_Mage_Helper_Data extends Mage_Core_Helper_Abstract{
+	
+	/**
+	 * Everything is worth a test :)
+	 *  <?php echo Mage::helper('Jpc_Mage')->debug(); ?>
+	 */
+	function debug(){
+		return 'Jpc Mage helper Class is connected.';
+	}
+	
+	/**
+	 * Custom Functions Here
+	 */
+	 
+}
