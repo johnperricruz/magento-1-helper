958d26e5bde00fed69d5f0d3e262b3975d682170
John Perri Cruz <jcruz@optimizex.com>
2018-08-20 14:03:35 -0700

    Features : Added Catalog Functions

--- app/code/local/Jpc/Mage/Helper/Data.php
+++ app/code/local/Jpc/Mage/Helper/Data.php
@@ -13,9 +13,36 @@ class Jpc_Mage_Helper_Data extends Mage_Core_Helper_Abstract{
 	/**
 	 * Custom Functions Here
 	 */
-	 function getStaticBlock($_this,$identifier){
-		 //Mage::helper('Jpc_Mage')->getStaticBlock($this,'left')
-		 return $_this->getLayout()->createBlock('cms/block')->setBlockId($identifier)->toHtml();
-	 }
-	 
-}
+	function getStaticBlock($identifier){
+		//Mage::helper('Jpc_Mage')->getStaticBlock('left')
+		return $this->getLayout()->createBlock('cms/block')->setBlockId($identifier)->toHtml();
+	}
+	function getSkinImageUrl($location){
+		////Mage::helper('Jpc_Mage')->getSkinUrl('folder/logo.png')
+		return Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_SKIN).'/frontend/rwd/rocket/images/'.$location;
+	}
+	function getRevSlider($identifier){
+		//Mage::helper('Jpc_Mage')->getRevSlider('homepage-slider') 
+		return $this->getLayout()->createBlock('nwdrevslider/revslider')->setAlias($identifier)->toHtml();
+	}
+	
+	/**
+	 * Products & Categories
+	 */
+	function getProductsInCategory($cat_id){
+		//Mage::helper('Jpc_Mage')->getProductsInCategory(1) 
+		$category_model = Mage::getModel('catalog/category')->load($category_id);    
+		$collection = Mage::getResourceModel('catalog/product_collection');
+		$collection->addCategoryFilter($category_model);  							 
+		$collection->addAttributeToFilter('status',1);                              
+		$collection->addAttributeToFilter('visibility',4); 
+		$collection->addAttributeToSelect(array('description','name','url','small_image','price')); 
+		//$collection->getSelect()->order('rand()');                                 
+		$collection->addStoreFilter(); 
+		return $collection; 
+	}
+	public function getProductImageUrl($prod,$size){
+		//Mage::helper('Jpc_Mage')->getProductImageUrl($prod,$size = 200) 
+		return Mage::helper('catalog/image')->init($prod, 'small_image')->resize($size);
+	}
+} 
