ffa91e57e27d8c90e4b5391d5e4d12d7ebe992c8
John Perri Cruz <jcruz@optimizex.com>
2018-08-23 16:41:49 -0700

    Updates : Utility & other helper methods

--- app/code/local/Jpc/Mage/Helper/Data.php
+++ app/code/local/Jpc/Mage/Helper/Data.php
@@ -25,6 +25,26 @@ class Jpc_Mage_Helper_Data extends Mage_Core_Helper_Abstract{
 		//Mage::helper('Jpc_Mage')->getRevSlider($this,'homepage-slider') 
 		return $_this->getLayout()->createBlock('nwdrevslider/revslider')->setAlias($identifier)->toHtml();
 	}
+	/**
+	 * Cart
+	 */
+	function getCartItems(){
+		//Mage::helper('Jpc_Mage')->getCartItems();
+		//$this->getProduct()->getName()
+		return Mage::getModel('checkout/cart')->getQuote()->getAllItems();
+	}
+	function isCart(){
+		//Mage::helper('Jpc_Mage')->isCart() 
+		if (Mage::app()->getFrontController()->getAction()->getFullActionName() == 'checkout_cart_index') {
+			return true;
+		} 
+		return false;
+	} 
+	 
+	 
+	/**
+	 * Product & Category
+	 */
 	function getProductsInCategory($cat_id){
 		//Mage::helper('Jpc_Mage')->getProductsInCategory(1) 
 		$category_model = Mage::getModel('catalog/category')->load($category_id);    
@@ -41,4 +61,33 @@ class Jpc_Mage_Helper_Data extends Mage_Core_Helper_Abstract{
 		//Mage::helper('Jpc_Mage')->getProductImageUrl($prod,$size = 200) 
 		return Mage::helper('catalog/image')->init($prod, 'small_image')->resize($size);
 	}
+	
+	/**
+	 * Utilities
+	 */
+	function getWishlist(){
+		return  Mage::helper('wishlist')->getWishlistItemCollection();
+	}
+	function isWishlistEmpty(){
+		// Mage::helper('Jpc_Mage')->isWishlistEmpty() 
+		if(Mage::getSingleton('customer/session')->getWishlistItemCount() >= 1){
+			return true;
+		}
+		return false;
+	}
+	function strTruncate($str,$limit){
+		// Mage::helper('Jpc_Mage')->strTruncate($str,$limit = 50) 
+		if(strlen($str) >= $limit){
+			return substr($str,0,$limit).'...';
+		}
+		return $str;
+	}
+	function getStoreCurrency(){
+		// Mage::helper('Jpc_Mage')->getStoreCurrency() 
+		return Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol();
+	}
+	function getFormKey(){
+		// Mage::helper('Jpc_Mage')->getFormKey() 
+		return Mage::getSingleton('core/session')->getFormKey();
+	}	 
 } 
